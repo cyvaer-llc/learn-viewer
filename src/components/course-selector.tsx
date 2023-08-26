63b53d31ed8bf531fe1d84f72635161a90bd7a5c
Matt McKnett <mmcknett@gmail.com>
2023-08-25 17:39:36 -0700

    Course selector spins during load

--- src/components/course-selector.tsx
+++ src/components/course-selector.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import './course-selector.css';
 import Combobox from 'react-widgets/Combobox';
-import { CourseSettersContext } from '../contexts/current-course';
+import { CourseSettersContext, CourseStateContext } from '../contexts/current-course';
 import { CourseModel } from '../models/course';
 
 type CourseSelectorProps = {
@@ -35,8 +35,8 @@ const DEFAULT_COURSE = courseFromQuery() || KNOWN_COURSE_URLS[0][1];
 
 export default function CourseSelector(_props: CourseSelectorProps) {
   const [ course, setCourse ] = useState(DEFAULT_COURSE);
-  console.log(course, DEFAULT_COURSE);
   const { setCourseLoading, setCourse: setCourseModel, setCourseError } = useContext(CourseSettersContext)!;
+  const courseState = useContext(CourseStateContext)!;
 
   const loadCourseData = (courseUrl: string): () => void => {
     const abortController = new AbortController();
@@ -111,6 +111,7 @@ export default function CourseSelector(_props: CourseSelectorProps) {
             <div className='course-url'>{item[1]}</div>
           </div>
         )}
+        busy={ courseState.loading }
       />
       <button type="submit">Load</button>
     </form>
