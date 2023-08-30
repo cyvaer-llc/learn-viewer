4564249becb6b88162cfc829606d34ab4a057db0
Matt McKnett <mmcknett@gmail.com>
2023-08-30 11:47:30 -0700

    Add correct answer info in challenge state & reducer

--- src/reducers/challenge-reducer.ts
+++ src/reducers/challenge-reducer.ts
@@ -5,6 +5,8 @@ type ChallengeOption = string;
 export type ChallengeOptionConfiguration = {
   possibleOptions: Set<ChallengeOption>;
   selectedOptions: Set<ChallengeOption>;
+  correctOptions?: Set<ChallengeOption>;
+  lastAttempt: 'unanswered' | 'correct' | 'wrong';
 }
 
 export type ChallengeState = {
@@ -25,6 +27,7 @@ export type ChallengeAction = {
 export type ChallengeMutators = {
   toggleOption: (optionId: ChallengeOption) => void;
   setPossibilities: (options: ChallengeOption[]) => void;
+  checkAnswer: (challengeId: string) => void;
 }
 
 const set = (options: ChallengeOption[]): ChallengeAction => {
@@ -57,6 +60,15 @@ const toggle = (optionId: ChallengeOption): ChallengeAction => {
   };
 };
 
+const check = (challengeId: string): ChallengeAction => {
+  return {
+    type: 'CHECK_ANSWER',
+    payload: {
+      challengeId
+    }
+  }
+};
+
 function reducer(state: ChallengeState = INITIAL_STATE, action: ChallengeAction): ChallengeState {
   switch (action.type) {
     case 'TOGGLE_OPTION': {
@@ -72,16 +84,38 @@ function reducer(state: ChallengeState = INITIAL_STATE, action: ChallengeAction)
         [challengeId]: {
           ...state[challengeId],
           selectedOptions: challengeOptions,
+          lastAttempt: 'unanswered'
         }
       };
     }
     case 'SET_POSSIBILITIES': {
       const { challengeId, options } = action.payload;
+      // TODO: Support persisting challenge state. As-is, we'll reset
+      //       challenges on every re-render.
       return {
         ...state,
         [challengeId]: {
           possibleOptions: new Set<ChallengeOption>(options),
           selectedOptions: new Set<ChallengeOption>(),
+          lastAttempt: 'unanswered'
+        }
+      };
+    }
+    case 'CHECK_ANSWER': {
+      const { challengeId } = action.payload;
+      const challenge = state[challengeId];
+      const correctOptions = challenge.correctOptions;
+      if (!correctOptions) {
+        throw new Error('Cannot check answers for a challenge without a list of correct options.');
+      }
+
+      const selectedOptions = challenge.selectedOptions;
+      const isCorrect = correctOptions && selectedOptions.size === correctOptions.size && [...selectedOptions].every(option => correctOptions.has(option));
+      return {
+        ...state,
+        [challengeId]: {
+          ...challenge,
+          lastAttempt: isCorrect ? 'correct' : 'wrong'
         }
       };
     }
@@ -100,7 +134,10 @@ export function useChallengeReducer(): [ChallengeState, ChallengeMutators] {
       },
       setPossibilities: (options: ChallengeOption[]) => {
         dispatch(set(options));
-      }
+      },
+      checkAnswer: (challengeId: string) => {
+        dispatch(check(challengeId));
+      },
     };
     return mutators;
   }, []);
